92bd45124e4c28858aeec24a7e633a8a72d25a4e
balaji379 <vigneshbalaji.kvl@gmail.com>
2024-11-26 23:35:32 +0530

    code updated

--- userService/src/main/java/com/example/demo/services/ResultPublishService.java
+++ userService/src/main/java/com/example/demo/services/ResultPublishService.java
@@ -24,67 +24,72 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ResultPublishService {
-     
+
 	private final ResultPublishRepo resultPublishRepo;
 	private final EventRepo eventRepo;
 	private final AthleteRepo athleteRepo;
 	private final ImageCompressor compressor;;
 	private final AthleteService athleteService;
-	
+
 	public void saveResult(ResultPublishDto dto) {
 		ResultPublish eventResult = ResultPublish.builder()
-				                                 .firstPlayer(dto.getFirstPlayer())
-				                                 .secondPlayer(dto.getSecondPlayer())
-				                                 .eventId(dto.getEventId())
-				                                 .build();
+				.firstPlayer(dto.getFirstPlayer())
+				.secondPlayer(dto.getSecondPlayer())
+				.eventId(dto.getEventId())
+				.build();
 		resultPublishRepo.save(eventResult);
 	}
-	
-	public ResultPublishDto getEventResult (String eventid) {
+
+	public ResultPublishDto getEventResult(String eventid) {
 		ResultPublishDto resultPublishDto = new ResultPublishDto();
 		int eventId = Integer.parseInt(eventid);
 		if (resultPublishRepo.existsById(eventId)) {
-			 ResultPublish resultPublish = resultPublishRepo.findById(eventId).get();
-			 BeanUtils.copyProperties(resultPublish, resultPublishDto);
-		} 
+			ResultPublish resultPublish = resultPublishRepo.findById(eventId).get();
+			BeanUtils.copyProperties(resultPublish, resultPublishDto);
+		}
 		return resultPublishDto;
 	}
-	
-	public List<ResultPublishDto> getAllResult(){
-		List<ResultPublish> results =  resultPublishRepo.findAll();
+
+	public List<ResultPublishDto> getAllResult() {
+		List<ResultPublish> results = resultPublishRepo.findAll();
 		List<ResultPublishDto> resultEvent = new ArrayList<>();
+
 		for (ResultPublish result : results) {
-			    Event event2 = eventRepo.findById(result.getEventId()).get();
-			    event2.setImage(null);
-			    ResultPublishDto dto = new ResultPublishDto();
-			    BeanUtils.copyProperties(result, dto);
-			    dto.setEvents(event2);
-			    resultEvent.add(dto);
+
+			Event event2 = eventRepo.findById(result.getEventId()).orElse(null);
+			if (event2 != null) {
+				event2.setImage(null);
+				ResultPublishDto dto = new ResultPublishDto();
+				BeanUtils.copyProperties(result, dto);
+				dto.setEvents(event2);
+				resultEvent.add(dto);
+			}
 		}
+
 		return resultEvent;
 	}
 
 	public List<AthleteData> getWinner(int eventId) throws IOException, java.io.IOException {
 		List<AthleteData> winners = new ArrayList<>();
 		if (resultPublishRepo.existsById(eventId)) {
-	      		final ResultPublish result = resultPublishRepo.findById(eventId).orElse(null);
-	      		if (result != null && athleteRepo.existsByAthleteId(result.getFirstPlayer())) {
-	      			AthleteData fp = athleteService.getAthleteData(result.getFirstPlayer());
-//	      			fp.setBase64Image(getBase64(fp.getPhoto()));
-	      			winners.add(fp);
-	      		}
-	      		if (result != null && athleteRepo.existsByAthleteId(result.getSecondPlayer())) {
-	      			AthleteData sp = athleteService.getAthleteData(result.getSecondPlayer());
-//	      			sp.setBase64Image(getBase64(sp.getPhoto()));
-	      			winners.add(sp);
-	      			
-	      		}
+			final ResultPublish result = resultPublishRepo.findById(eventId).orElse(null);
+			if (result != null && athleteRepo.existsByAthleteId(result.getFirstPlayer())) {
+				AthleteData fp = athleteService.getAthleteData(result.getFirstPlayer());
+				// fp.setBase64Image(getBase64(fp.getPhoto()));
+				winners.add(fp);
+			}
+			if (result != null && athleteRepo.existsByAthleteId(result.getSecondPlayer())) {
+				AthleteData sp = athleteService.getAthleteData(result.getSecondPlayer());
+				// sp.setBase64Image(getBase64(sp.getPhoto()));
+				winners.add(sp);
+
+			}
 		}
 		return winners;
 	}
-	
+
 	private String getBase64(byte[] image) {
 		return Base64.getEncoder().encodeToString(image);
 	}
-	 
+
 }
